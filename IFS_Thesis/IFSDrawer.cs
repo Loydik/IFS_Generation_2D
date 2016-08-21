87918aa36403d0245e9863ecc2c6de7d1d9805f1
Vladyslav Babych <vladik100x@gmail.com>
2016-08-21 19:27:28 +0200

    initial commit

---
+++ IFS_Thesis/IFSDrawer.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+
+namespace IFS_Thesis
+{
+    public class IfsDrawer
+    {
+        public void SaveIfsImage(List<double[]> ifsMappings, int imgx, int imgy, string path)
+        {
+            var bmp = DrawIfs(ifsMappings, imgx, imgy);
+            bmp.Save(path, ImageFormat.Png);
+            bmp.Dispose();
+        }
+
+        public Bitmap DrawIfs(List<double[]> ifsMappings, int imgx, int imgy)
+        {
+            var randomGen = new Random();
+
+            var length = ifsMappings.Count;
+
+            var x = ifsMappings[0][4];
+            var y = ifsMappings[0][5];
+            var xa = x;
+            var xb = x;
+            var ya = y;
+            var yb = y;
+
+            for (int k = 0; k < imgx*imgy; k++)
+            {
+                var p = randomGen.NextDouble();
+                var psum = 0.0;
+
+                var i = 0;
+
+                for (int j = 0; j < length; j++)
+                {
+                    psum += ifsMappings[j][6];
+
+                    i = j;
+
+                    if (p <= psum)
+                        break;
+                    }
+
+                var x0 = x*ifsMappings[i][0] + y*ifsMappings[i][1] + ifsMappings[i][4];
+                    y = x*ifsMappings[i][2] + y*ifsMappings[i][3] + ifsMappings[i][5];
+                    x = x0;
+
+                    if (x < xa)
+                        xa = x;
+                    if (x > xb)
+                        xb = x;
+                    if (y < ya)
+                        ya = y;
+                    if (y > yb)
+                        yb = y;
+                }
+
+            imgy = Convert.ToInt32(imgy * (yb - ya) / (xb - xa)); //auto-re-adjust the aspect ratio
+
+            var bmpImage = DrawFilledRectangle(imgx, imgy);
+
+            x = 0.0;
+            y = 0.0;
+
+            for (int k = 0; k < imgx * imgy; k++)
+            {
+                var p = randomGen.NextDouble();
+                var psum = 0.0;
+
+                var i = 0;
+
+                for (int j = 0; j < length; j++)
+                {
+                    psum += ifsMappings[j][6];
+
+                    i = j;
+
+                    if (p <= psum)
+                        break;
+                }
+
+                var x0 = x * ifsMappings[i][0] + y * ifsMappings[i][1] + ifsMappings[i][4];
+                    y = x * ifsMappings[i][2] + y * ifsMappings[i][3] + ifsMappings[i][5];
+                    x = x0;
+
+                    var jx = Convert.ToInt32((x - xa)/(xb - xa)*(imgx - 1));
+                    var jy = (imgy - 1) - Convert.ToInt32((y - ya)/(yb - ya)*(imgy - 1));
+
+                    bmpImage.SetPixel(jx, jy, Color.Black);
+             }
+            
+
+            return bmpImage;
+        }
+
+        private Bitmap DrawFilledRectangle(int x, int y)
+        {
+            Bitmap bmp = new Bitmap(x, y);
+
+            using (Graphics graph = Graphics.FromImage(bmp))
+            {
+                Rectangle imageSize = new Rectangle(0, 0, x, y);
+                graph.FillRectangle(Brushes.White, imageSize);
+            }
+            return bmp;
+        }
+    }
+}
---
+++ IFS_Thesis/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IFS_Thesis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var imageSavePath = "C:/Users/Loydik94/Desktop/IFS Images/ifs.png";
+
+            int sizeX = 512;
+            int sizeY = 512;
+
+            List<double[]> fern = new List<double[]> {new[] { 0.0, 0.0, 0.0, 0.16, 0.0, 0.0, 0.01 }, new[] { 0.85, 0.04, -0.04, 0.85, 0.0, 1.6, 0.85 }, new[] { 0.2, -0.26, 0.23, 0.22, 0.0, 1.6, 0.07 }, new[] { -0.15, 0.28, 0.26, 0.24, 0.0, 0.44, 0.07 }};
+
+            List<double[]> pentagon = new List<double[]> { new[] { 0.382, 0.0, 0.0, 0.382, 0.0, 0.0, 0.2 }, new[] { 0.382, 0.0, 0.0, 0.382, 0.618, 0.0, 0.2 }, new[] { 0.382, 0.0, 0.0, 0.382, 0.809, 0.588, 0.2 }, new[] { 0.382, 0.0, 0.0, 0.382, 0.309, 0.951, 0.2 }, new[] { 0.382, 0.0, 0.0, 0.382, -0.191, 0.588, 0.2 } };
+
+            var drawer = new IfsDrawer();
+            drawer.SaveIfsImage(fern, sizeX, sizeY, imageSavePath);
+
+
+        }
+    }
+}
